35600c25e70e6ae9e8fa7357279bde6055240437
Sumati Jha <sjha@himalayantechies.com>
2018-05-03 08:40:05 +0000

    Contact Page layout

---
+++ webapp/Component.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"LicensedToCode/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("LicensedToCode.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+			//Initialize the Router
+			this.getRouter().initialize();
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+		}
+	});
+});
